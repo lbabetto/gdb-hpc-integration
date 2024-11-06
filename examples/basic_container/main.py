c8eb510a3e148bcf066dd703f631d58b58a083c4
kazhamiakin <kazhamiakin@gmail.com>
2024-11-06 13:20:58 +0100

    Docker added

---
+++ examples/basic_container/main.py
@@ -0,0 +1,9 @@
+import argparse
+from src.process import process_data
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str)
+    parser.add_argument('--output', type=str)
+    args = parser.parse_args()
+    process_data(args.input, args.output)
