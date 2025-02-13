409b05d85101edcaec8a64352d573da79678f5b8
lbabetto <l.babetto@cineca.it>
2025-02-13 15:02:46 +0100

    Bugfix

--- examples/basic_container/src/process.py
+++ examples/basic_container/src/process.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from os.path import splitext
 
+
 def process_data(files_list):
     for file in files_list:
         df = pd.read_parquet(f"/input/{file}")
-        df.to_json(f"/output/{splitext(file)}.json", orient="records")
-    
+        df.to_json(f"/output/{splitext(file)[0]}.json", orient="records")
